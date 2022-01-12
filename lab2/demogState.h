1afcf5a56aeda5b4a9f57d30e20cc802c67cbb3f
emilyt <etruon08@calpoly.edu>
2022-01-12 12:02:19 -0800

    defined private vars for demogState

--- lab2/demogState.h
+++ lab2/demogState.h
@@ -16,6 +16,18 @@ class demogState {
    demogState(){}
   friend std::ostream& operator<<(std::ostream &out, const demogState &SD);
 	private:
+        const string county;
+        const string state;
+        const int population;
+        const string race
+        const string ethnicity;
+        const float medIncome;
+        const int homeOwn; //check if right
+        const int personPerHouse;
+        const int veterans;
+        const int highSchoolGrad;
+        const int bachelorsDeg;
+        const int foreignBorn;
 	//DATA here
 };
 #endif
